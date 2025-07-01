af39110526fff3d7bbd1d5ae2637cfad79e890fb
Aaron Gibran <aarongibran13@gmail.com>
2025-07-01 13:12:30 -0300

    feature:criação da SerchBar atraves de componentes do MuiMaterialDesing

---
+++ recomenda-livros/src/components/Forms/SearchBar.tsx
@@ -0,0 +1,51 @@
+"use client";  
+import React from 'react';
+import { InputBase, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
+
+const SearchContainer = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: 'white',
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#90a4ae', // Cor do ícone de busca
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+}));
+
+export default function SearchBar() {
+  return (
+    <SearchContainer>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Buscar Livro..."
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </SearchContainer>
+  );
+}
