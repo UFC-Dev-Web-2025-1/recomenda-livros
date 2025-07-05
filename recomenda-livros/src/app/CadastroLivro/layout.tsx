abf7fe8bdd8ff2951833494cdccdbc0304b143c3
Seu Nome <seuemail@exemplo.com>
2025-07-05 10:15:40 -0300

    manutenção

--- recomenda-livros/src/app/CadastroLivro/layout.tsx
+++ recomenda-livros/src/app/CadastroLivro/layout.tsx
@@ -1,15 +1,24 @@
-// layout.tsx
-import './globals.css';
+// app/selecionar-livro/layout.tsx
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Selecionar Livro',
-  description: 'Adicione livros à sua estante',
+  title: "Selecionar Livro",
+  description: "Adicione livros à sua estante",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function SelecionarLivroLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <main className="main-content">{children}</main>
+      </body>
     </html>
   );
 }
