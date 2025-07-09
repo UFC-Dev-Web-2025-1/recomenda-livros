ba8564f57303f7b9e712081da09718669b51ed57
Aaron Gibran <aarongibran13@gmail.com>
2025-07-09 09:51:38 -0300

    refactor: removendo comentarios ja repassado a equipe

--- recomenda-livros/src/components/Cards/BookCard/BookCover.tsx
+++ recomenda-livros/src/components/Cards/BookCard/BookCover.tsx
@@ -12,20 +12,20 @@ export default function BookCover({ src, alt }: BookCoverProps) {
   return (
     <Box
       sx={{
-        width: { xs: '100px', sm: '120px', md: '140px' }, // Largura responsiva
-        height: { xs: '150px', sm: '180px', md: '210px' }, // Altura responsiva (proporção aprox. 1.5)
-        borderRadius: '8px', // Bordas arredondadas
-        overflow: 'hidden', // Garante que a imagem se ajuste ao borderRadius
-        flexShrink: 0, // Impede que a imagem encolha em layouts flexíveis
+        width: { xs: '100px', sm: '120px', md: '140px' }, 
+        height: { xs: '150px', sm: '180px', md: '210px' }, 
+        borderRadius: '8px', 
+        overflow: 'hidden', 
+        flexShrink: 0, 
       }}
     >
       <Image
         src={src}
         alt={alt}
-        width={140} // Largura intrínseca para otimização do Next.js
-        height={210} // Altura intrínseca para otimização do Next.js
-        style={{ objectFit: 'cover', width: '100%', height: '100%' }} // Estilo para preencher o Box
-        priority // Considerar usar se for a imagem principal da tela
+        width={140} 
+        height={210} 
+        style={{ objectFit: 'cover', width: '100%', height: '100%' }} 
+        priority 
       />
     </Box>
   );
