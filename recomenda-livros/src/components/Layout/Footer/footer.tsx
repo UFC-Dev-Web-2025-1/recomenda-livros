5a07347700c2ec454feac485c48927f3be1387c5
Aaron Gibran <aarongibran13@gmail.com>
2025-07-21 21:34:05 -0300

    refactor:adicionando routas e configurações de navegação

--- recomenda-livros/src/components/Layout/Footer/footer.tsx
+++ recomenda-livros/src/components/Layout/Footer/footer.tsx
@@ -2,16 +2,18 @@
 
 import React from 'react';
 import styles from './footer.module.css';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Box className={styles.footerItem}>
+      <Box className={styles.footerItem}  >
         <span className={styles.forumBadge}>32</span>
-        Fóruns
+          <Link href="/Forum">Fóruns</Link>
+        
       </Box>
       <Box className={styles.footerItem}>
         Recomendações
--- recomenda-livros/src/components/Navigation/Slidebar.tsx
+++ recomenda-livros/src/components/Navigation/Slidebar.tsx
@@ -46,7 +46,7 @@ export default function Sidebar({ isVisible, onLinkClick }: SidebarProps) {
     { text: 'Minha Estante', href: '/MinhaEstante' },
     { text: 'Selecionar Livro', href: '/SelecionarLivro' },
     { text: 'Principal de Estatísticas', href: '/Estatisticas' },
-    { text: 'Perfil', href: '/ConfiguracoesPerfil' },
+    { text: 'Perfil', href: '/Perfil' },
     { text: 'Login', href: '/' },
     { text: 'Sair', onClick: handleLinkClick, href: '/' },
   ];
