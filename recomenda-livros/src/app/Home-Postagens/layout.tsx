51ec5d7d92a308ee2b4c821bfcac5c1b477fbf63
Aaron Gibran <aarongibran13@gmail.com>
2025-08-01 16:57:42 -0300

    bugfix: Corrigindo bug da tela de Home

--- recomenda-livros/src/app/Home-Postagens/layout.tsx
+++ recomenda-livros/src/app/Home-Postagens/layout.tsx
@@ -9,16 +9,8 @@ import Footer from '@/src/components/Layout/Footer/footer';
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <html lang="pt-BR">
-                <body>
-
-                    
-                    <Footer />
-
-                    { children}
-
-                </body>
-            </html>
+            <Footer />
+            {children}
         </>
     );
 }
